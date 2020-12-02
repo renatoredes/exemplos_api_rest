0c81e9583468feaddc806eded2a604e60a099e89
DESKTOP-PRT6VSO\Renato <redes>
2020-12-02 16:52:42 -0300

    crud utilizando JPA

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,9 @@
-insert into medico (id, nome) values (1, 'Lucas');
-insert into medico (id, nome) values (2, 'Beatriz');
+insert into medico (id, contato,crm,especialidade_medica, nome) values (1, '(81)9.8888-7777','123','Pediatra','Lucas');
+insert into medico (id, contato,crm,especialidade_medica, nome) values (2, '(81)9.8888-7777','123','Pediatra','Beatriz');
 
-insert into consulta (id, nome, valor_consulta, id_medico) values (1, 'Pediatra', 700, 1);
-insert into consulta (id, nome, valor_consulta, id_medico) values (2, 'Pediatra', 900.00, 1);
-insert into consulta (id, nome, valor_consulta, id_medico) values (3, 'Pediatra', 500, 2);
+
+insert into consulta (id, descricao_consulta, valor_consulta, id_medico) values (1, 'paciente estar bem', 700, 1);
+insert into consulta (id, descricao_consulta, valor_consulta, id_medico)  values (2, 'paciente estar bem', 900.00, 1);
+insert into consulta (id, descricao_consulta, valor_consulta, id_medico)  values (3, 'paciente estar bem', 500, 2);
 
 
--- target/classes/import.sql
+++
@@ -1,8 +0,0 @@
-insert into medico (id,contato,crm,especialidade_medica, nome) values (1,'(81) 9.7777-8888','1234','Pediatria','Lucas');
-insert into medico (id,contato,crm,especialidade_medica, nome) values (2,'(81) 9.7777-5588','1234','Pediatria','Beatriz');
-
-insert into consulta (id, descricao_consulta, valor_consulta, id_medico) values (1, 'Paciente estar 100% para trabalhar', 80, 1);
-
-
-
-
