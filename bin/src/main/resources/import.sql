a2718998320abd5ececb7ab151aa5ab2aab5ec0d
DESKTOP-PRT6VSO\Renato <redes>
2020-12-09 22:12:46 -0300

    remover bin

--- bin/src/main/resources/import.sql
+++
@@ -1,8 +0,0 @@
-insert into medico (id, nome) values (1, 'Lucas');
-insert into medico (id, nome) values (2, 'Beatriz');
-
-insert into consulta (id, nome, valor_consulta, medico_id) values (1, 'Pediatra', 700, 1);
-insert into consulta (id, nome, valor_consulta, medico_id) values (2, 'Pediatra', 900.00, 1);
-insert into consulta (id, nome, valor_consulta, medico_id) values (3, 'Pediatra', 500, 2);
-
-
