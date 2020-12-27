0215ae1b3566069e4d68c90bdd54c4d19156a1c6
DESKTOP-PRT6VSO\Renato <redes>
2020-12-27 12:22:55 -0300

    projeto limpo

---
+++ src/main/java/br/com/clinica/ClinicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.clinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaApplication {
+
+	public static void main(String[] args) {		
+		SpringApplication.run(ClinicaApplication.class, args);
+	}
+
+}
