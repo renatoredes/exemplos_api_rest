828d68b830c1c5753ea527bd9d6f2a71ec8b86f1
DESKTOP-PRT6VSO\Renato <redes>
2021-01-30 10:56:28 -0300

    implementação get-id put e delete

--- src/main/java/br/com/clinica/controller/MedicoController.java
+++ src/main/java/br/com/clinica/controller/MedicoController.java
@@ -1,13 +1,17 @@
 package br.com.clinica.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 
+import br.com.clinica.domain.exeption.EntityNotFoundException;
 import br.com.clinica.dto.request.MedicoDTO;
 import br.com.clinica.dto.response.MessageResponseDTO;
 import br.com.clinica.service.MedicoService;
@@ -36,5 +40,20 @@ public class MedicoController {
     @GetMapping
     public List<MedicoDTO> listAll() {
         return medicoService.listAll();
+    }
+    @GetMapping("/{id}")
+    public MedicoDTO findById(@PathVariable Long id) throws EntityNotFoundException {
+        return medicoService.findById(id);
+    }
+    
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid MedicoDTO medicoDTO) throws EntityNotFoundException {
+        return medicoService.updateById(id, medicoDTO);
+    }
+    
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws EntityNotFoundException {
+    	medicoService.delete(id);
     }
 }
--- src/main/java/br/com/clinica/service/MedicoService.java
+++ src/main/java/br/com/clinica/service/MedicoService.java
@@ -1,20 +1,20 @@
 package br.com.clinica.service;
 
-import org.springframework.stereotype.Service;
-
-
-import br.com.clinica.domain.model.Medico;
-import br.com.clinica.dto.request.MedicoDTO;
-import br.com.clinica.dto.response.MessageResponseDTO;
-import br.com.clinica.mapper.LaudoMedicoMapper;
-import br.com.clinica.mapper.MedicoMapper;
-import br.com.clinica.repository.MedicoRepository;
-import lombok.AllArgsConstructor;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.clinica.domain.exeption.EntityNotFoundException;
+import br.com.clinica.domain.model.Medico;
+import br.com.clinica.dto.request.MedicoDTO;
+import br.com.clinica.dto.response.MessageResponseDTO;
+import br.com.clinica.mapper.MedicoMapper;
+import br.com.clinica.repository.MedicoRepository;
+import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -39,11 +39,37 @@ public class MedicoService {
 	                .collect(Collectors.toList());
 	 }
 	 
-	 private MessageResponseDTO createMessageResponse(Long id, String message) {
-	        return MessageResponseDTO
-	                .builder()
-	                .message(message + id)
-	                .build();
-	    }
+	 
+	public MedicoDTO findById(Long id) throws EntityNotFoundException {
+		Medico medico = vericaIfExiste(id);
+		return medicoMapper.toDTO(medico);
+	}
 
+
+	public MessageResponseDTO updateById(Long id, @Valid MedicoDTO medicoDTO) throws EntityNotFoundException {
+		vericaIfExiste(id);
+		Medico medicoToUpdate = medicoMapper.toModel(medicoDTO);
+	
+		Medico updateMedico = medicoRepository.save(medicoToUpdate);
+		return createMessageResponse(updateMedico.getId(), "Updated person with ID ");
+		
+	}
+
+	private Medico vericaIfExiste(Long id) throws EntityNotFoundException {
+		return medicoRepository.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException(id));
+	}
+	
+	private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
+    }
+
+	public void delete(Long id) throws EntityNotFoundException{
+		vericaIfExiste(id);
+		medicoRepository.deleteById(id);
+	}
+	
 }
